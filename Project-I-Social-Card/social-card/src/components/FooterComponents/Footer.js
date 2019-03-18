6184df12cd35b45ac5fcc6f8fd92aceaf80d60f4
Mckay <mckaywrigley@gmail.com>
2019-03-18 12:57:23 -0700

    adds Footer components

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <i class="far fa-comment first"></i>
+            <i class="fas fa-retweet"></i><p>6</p>
+            <i class="far fa-heart"></i><p>4</p>
+            <i class="far fa-envelope"></i>
+        </div>
+    );
+}
+
+export default Footer;
