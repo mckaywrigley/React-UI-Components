9aa72f06e25d04ec315aa20c6764e21ff7984cd4
Mckay <mckaywrigley@gmail.com>
2019-03-18 12:56:46 -0700

    adds Card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+        <div className="card-content">
+            <h2>Get started with React!</h2>
+            <p>This is a starter guide for building your first React app.</p>
+            <a href='https://www.reactjs.org'>react.js.org</a>
+        </div>
+    );
+}
+
+export default CardContent;
