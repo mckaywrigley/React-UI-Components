e0e086ea8dde62adc9ff9662247fe1be038d4fb5
Mckay <mckaywrigley@gmail.com>
2019-03-18 12:56:10 -0700

    adds Header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+            <h2>Lambda School</h2>
+            <p>@LambdaSchool</p>
+            <p>{moment().format("MMM Do YY")}</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
