54c09007683d7771cce3648aaa517dcaa66d7493
Mckay <mckaywrigley@gmail.com>
2019-03-19 08:56:59 -0700

    configures app

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
-    return (
-        <div className="footer">
-            <i class="far fa-comment first"></i>
-            <i class="fas fa-retweet"></i><p>6</p>
-            <i class="far fa-heart"></i><p>4</p>
-            <i class="far fa-envelope"></i>
-        </div>
-    );
+class Footer extends React.Component {
+
+    likeHandler = e => {
+        alert('liked');
+    }
+
+    render() {
+        return (
+            <div className="footer">
+                <i class="far fa-comment first"></i>
+                <i class="fas fa-retweet"></i><p>6</p>
+                <i onClick={this.likeHandler} class="far fa-heart"></i><p>4</p>
+                <i class="far fa-envelope"></i>
+            </div>
+        );
+    }
+    
+    
 }
 
 export default Footer;
